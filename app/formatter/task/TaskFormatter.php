1bff5a4b443f078f6a948132648ead1b7e23523b
Abu Sabir <sabir@shuttlebd.com>
2018-08-18 18:07:36 +0600

    if any index array then formater solution in comment

--- app/formatter/task/TaskFormatter.php
+++ app/formatter/task/TaskFormatter.php
@@ -7,7 +7,7 @@ class TaskFormatter{
          return $tasks->map(function($task){
             return (object)[
                 'name' => $task->name.' working',
-                'desc' => $task->name,
+                'desc' => $task->name,//if you have any index array you can call another formatter to fittler this index array
             ];
         });
     }
